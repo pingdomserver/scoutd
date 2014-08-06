9f2595875053211688a63c0d7d0a51d2d9785ed9
Mr. Matt <matt@composition9.com>
2014-08-05 20:56:10 -0600

    realtime listening

--- scoutd.go
+++ scoutd.go
@@ -1,25 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 	"os/exec"
 	"sync"
 	"time"
+
+	"github.com/oguzbilgic/pusher"
 )
 
 func main() {
 	var wg sync.WaitGroup
 	wg.Add(2)
-	go listenForRealtime(&wg)
-	go reportLoop(&wg)
+	accountKey := os.Getenv("SCOUT_KEY")
+	go listenForRealtime(accountKey, &wg)
+	go reportLoop(accountKey, &wg)
 	wg.Wait()
 }
 
-func reportLoop(wg *sync.WaitGroup) {
+func reportLoop(accountKey string, wg *sync.WaitGroup) {
 	c := time.Tick(1 * time.Second)
 	for _ = range c {
 		// fmt.Printf("report loop\n")
-		cmd := exec.Command("ruby -s http://staging.scoutapp.com oh5inzUpIFFPgBErH719ff0IGLP7vSAhmdeakONI")
+		scoutGemBinPath := os.Getenv("SCOUT_GEM_BIN_PATH")
+		cmd := exec.Command(scoutGemBinPath, accountKey)
 		err := cmd.Run()
 		if err != nil {
 			log.Fatal(err)
@@ -28,7 +34,24 @@ func reportLoop(wg *sync.WaitGroup) {
 	wg.Done()
 }
 
-func listenForRealtime(wg *sync.WaitGroup) {
-	// add realtime listener
+func listenForRealtime(accountKey string, wg *sync.WaitGroup) {
+	conn, err := pusher.New("f07eaa39898f3c36c8cf")
+	if err != nil {
+		panic(err)
+	}
+
+	hostName := os.Getenv("SCOUT_HOSTNAME")
+	commandChan := conn.Channel(accountKey + "-" + hostName)
+	messages := commandChan.Bind("streamer_command") // a go channel is returned
+
+	for {
+		msg := <-messages
+		fmt.Printf("scout realtime " + msg.(string))
+		cmd := exec.Command("scout", "realtime", msg.(string))
+		err := cmd.Run()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 	wg.Done()
 }
