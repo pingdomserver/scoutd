d50903082237c25f59e5bbc0bf76256dda147e12
David Anderson <dave@dubkat.com>
2014-08-14 22:54:14 -0600

    htpclient setup, proxy support, check key against server

--- scoutd/config.go
+++ scoutd/config.go
@@ -2,6 +2,8 @@ package scoutd
 
 import (
 	"log"
+	"net/http"
+	"net/url"
 	"os"
 	"github.com/kylelemons/go-gypsy/yaml"
 	"github.com/imdario/mergo"
@@ -26,6 +28,10 @@ type ScoutConfig struct {
 	ReportingServerUrl string
 	PassthroughOpts []string
 	SubCommand string
+	HttpClients struct {
+		HttpClient *http.Client
+		HttpsClient *http.Client
+	}
 }
 
 func LoadConfig(cfg *ScoutConfig) {
@@ -41,10 +47,10 @@ func LoadConfig(cfg *ScoutConfig) {
 		configFile = defaults.ConfigFile
 	}
 	ymlOpts := LoadConfigFile(configFile) // load the options set in the config file
-	log.Printf("Defaults: %#v\n", defaults)
-	log.Printf("envOpts: %#v\n", envOpts)
-	log.Printf("cliOpts: %#v\n", cliOpts)
-	log.Printf("ymlOts: %#v\n", ymlOpts)
+	//log.Printf("Defaults: %#v\n", defaults)
+	//log.Printf("envOpts: %#v\n", envOpts)
+	//log.Printf("cliOpts: %#v\n", cliOpts)
+	//log.Printf("ymlOts: %#v\n", ymlOpts)
 	if err := mergo.Merge(cfg, defaults); err != nil {
 		log.Fatalf("Error while merging default config options: %s\n", err)
 	}
@@ -66,7 +72,8 @@ func LoadConfig(cfg *ScoutConfig) {
 		cfg.PassthroughOpts = append(cfg.PassthroughOpts, "-d", cfg.AgentDataFile)
 	}
 
-	log.Printf("Effective configuration: %#v\n", cfg)
+	//log.Printf("Effective configuration: %#v\n", cfg)
+	LoadHttpClients(cfg)
 }
 
 func LoadDefaults() (cfg ScoutConfig) {
@@ -130,4 +137,34 @@ func LoadConfigFile(configFile string) (cfg ScoutConfig) {
 	cfg.HttpsProxyUrl, err = conf.Get("https_proxy")
 	cfg.ReportingServerUrl, err = conf.Get("reporting_server_url")
 	return
+}
+
+func LoadHttpClients(cfg *ScoutConfig) {
+	var secTr, plainTr *http.Transport
+	var secProxyUrl, plainProxyUrl *url.URL
+	var err error
+
+	// set up the secure proxy and transport
+	if cfg.HttpsProxyUrl != "" {
+		secProxyUrl, err = url.Parse(cfg.HttpsProxyUrl)
+		if err != nil {
+			log.Fatalf("Error parsing HttpsProxyUrl: %s", err)
+		}
+	}
+	secTr = &http.Transport{
+		Proxy: http.ProxyURL(secProxyUrl),
+	}
+	cfg.HttpClients.HttpsClient = &http.Client{Transport: secTr}
+
+	// Set up the plain proxy and transport
+	if cfg.HttpProxyUrl != "" {
+		plainProxyUrl, err = url.Parse(cfg.HttpProxyUrl)
+		if err != nil {
+			log.Fatalf("Error parsing HttpProxyUrl: %s", err)
+		}
+	}
+	plainTr = &http.Transport{
+		Proxy: http.ProxyURL(plainProxyUrl),
+	}
+	cfg.HttpClients.HttpClient = &http.Client{Transport: plainTr}
 }
--- scoutd/helpers.go
+++ scoutd/helpers.go
@@ -1,19 +1,30 @@
 package scoutd
 
 import (
+	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"regexp"
 	"strings"
 )
 
-func AccountKeyValid(key string, checkServer bool) (bool, error) {
+func AccountKeyValid(key string, serverUrl string, client *http.Client) (bool, error) {
 	// Check the format of the account key - 40 chars, 0-9A-Za-z
 	matched, err := regexp.MatchString("^[0-9A-Za-z]{40}$", key)
 	if err != nil || !matched {
 		return false, err
+	} else if matched && serverUrl != "" {
+		json := fmt.Sprintf("{\"key\":\"%s\"}", key)
+		b := strings.NewReader(json)
+		resp, err := client.Post(serverUrl, "application/json", b)
+		if err != nil {
+			return false, err
+		} else if resp.StatusCode == 200 {
+			return true, err
+		}
 	}
-	return true, err
+	return false, err
 }
 
 func ShortHostname() string {
